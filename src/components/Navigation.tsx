1c416db260fa68241de47ccd8caf4cfaf2e61e9a
shaux <shaurya.sarma@gmail.com>
2022-07-18 12:37:46 -0400

    finished bottom navbar (haptic feedback)

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, TouchableOpacity, View } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import COLORS from "../res/colors/Colors";
@@ -9,14 +9,18 @@ import Tutorials from "../screens/Tutorials/index.Tutorials";
 import { AgendaIcon, LogbookIcon, PomodoroIcon, TutorialsIcon } from "./Icons";
 import generateBoxShadowStyle from "../../helpers/BoxShadow";
 import * as Haptics from "expo-haptics";
-import Ripple from "react-native-material-ripple";
+import React from "react";
 
+// bottom navbar
 const Tab = createBottomTabNavigator();
 // Screen names
 const pomodoroName = "Pomodoro";
 const agendaName = "Agenda";
 const tutorialsName = "Tutorials";
 const logbookName = "Logbook";
+// Styling constants
+const iconSize = 40;
+const iconColor = COLORS.purple;
 
 export default function Navigation() {
   return (
@@ -24,87 +28,66 @@ export default function Navigation() {
       <Tab.Navigator
         sceneContainerStyle={{ backgroundColor: COLORS.white }}
         initialRouteName="Pomodoro"
-        screenOptions={() => ({
+        screenOptions={({ route, navigation }) => ({
           tabBarShowLabel: false,
           headerShown: false,
           tabBarStyle: [styles.tabBar, styles.tabBarBoxShadow],
+          tabBarIcon: ({ focused }) => {
+            let rn = route.name;
+            let iconTag;
+            // assigning navigation icon based on name of route
+            if (rn === pomodoroName) {
+              // assigning JSX Tag
+              iconTag = (
+                <PomodoroIcon
+                  size={iconSize}
+                  fillColor={iconColor}
+                  isFocused={focused}
+                />
+              );
+            } else if (rn === agendaName) {
+              iconTag = (
+                <AgendaIcon
+                  size={iconSize}
+                  fillColor={iconColor}
+                  isFocused={focused}
+                />
+              );
+            } else if (rn === tutorialsName) {
+              iconTag = (
+                <TutorialsIcon
+                  size={iconSize}
+                  fillColor={iconColor}
+                  isFocused={focused}
+                />
+              );
+            } else if (rn === logbookName) {
+              iconTag = (
+                <LogbookIcon
+                  size={iconSize}
+                  fillColor={iconColor}
+                  isFocused={focused}
+                />
+              );
+            }
+            return (
+              <Pressable
+                onPress={() => {
+                  // add haptic feedback and navigate to new route
+                  Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+                  navigation.navigate(rn);
+                }}
+              >
+                {iconTag}
+              </Pressable>
+            );
+          },
         })}
       >
-        <Tab.Screen
-          name={pomodoroName}
-          component={Pomodoro}
-          options={{
-            tabBarIcon: ({ focused }) => {
-              return (
-                // <Ripple
-                //   style={styles.iconButton}
-                //   rippleColor={COLORS.purple}
-                //   rippleOpacity={0.3}
-                //   rippleDuration={500}
-                //   rippleCentered={true}
-                //   rippleContainerBorderRadius={100}
-                // >
-                <TouchableOpacity
-                  onPress={() => {
-                    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
-                  }}
-                >
-                  <PomodoroIcon
-                    size={40}
-                    fillColor={COLORS.purple}
-                    isFocused={focused}
-                  />
-                </TouchableOpacity>
-                // </Ripple>
-              );
-            },
-          }}
-        />
-        <Tab.Screen
-          name={agendaName}
-          component={Agenda}
-          options={{
-            tabBarIcon: ({ focused }) => {
-              return (
-                <AgendaIcon
-                  size={40}
-                  fillColor={COLORS.purple}
-                  isFocused={focused}
-                />
-              );
-            },
-          }}
-        />
-        <Tab.Screen
-          name={tutorialsName}
-          component={Tutorials}
-          options={{
-            tabBarIcon: ({ focused }) => {
-              return (
-                <TutorialsIcon
-                  size={40}
-                  fillColor={COLORS.purple}
-                  isFocused={focused}
-                />
-              );
-            },
-          }}
-        />
-        <Tab.Screen
-          name={logbookName}
-          component={Logbook}
-          options={{
-            tabBarIcon: ({ focused }) => {
-              return (
-                <LogbookIcon
-                  size={40}
-                  fillColor={COLORS.purple}
-                  isFocused={focused}
-                />
-              );
-            },
-          }}
-        />
+        <Tab.Screen name={pomodoroName} component={Pomodoro} />
+        <Tab.Screen name={agendaName} component={Agenda} />
+        <Tab.Screen name={tutorialsName} component={Tutorials} />
+        <Tab.Screen name={logbookName} component={Logbook} />
       </Tab.Navigator>
     </NavigationContainer>
   );
@@ -129,11 +112,4 @@ const styles = StyleSheet.create({
     5,
     COLORS.black10
   ),
-  iconButton: {
-    width: 50,
-    height: 50,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 50,
-  },
 });
