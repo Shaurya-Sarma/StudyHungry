40af6fcd22447f694f9d51ca87eaf0b0a5e3a8f4
shaux <shaurya.sarma@gmail.com>
2023-01-26 21:17:45 -0500

    increment build

--- src/api/YouTubeAPI.ts
+++ src/api/YouTubeAPI.ts
@@ -1,4 +1,4 @@
-import {API_KEY} from '@env'
+import { API_KEY } from "@env";
 
 const SEARCH_TERMS = [
   "study%20habits",
@@ -13,61 +13,67 @@ const SEARCH_TERMS = [
   "ted%20talk%20stress",
   "study%20vlog",
   "how%20to%20study%20smart",
-  "motivational%20speech"
+  "motivational%20speech",
 ];
 
 const NUM_OF_ENTRIES = 20;
 const SHORTEN_SUBTITLE = 40;
 
 export async function getYouTubeData(): Promise<any> {
-  // select a random key search term
-  let r = Math.floor(Math.random() * SEARCH_TERMS.length);
-  const RANDOM_KEYWORD = SEARCH_TERMS[r];
+  try {
+    // select a random key search term
+    let r = Math.floor(Math.random() * SEARCH_TERMS.length);
+    const RANDOM_KEYWORD = SEARCH_TERMS[r];
 
-  // fetch data from YouTube API
-  // based on relevance and top NUM_OF_ENTRIES
-  let data = await fetch(
-    "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=" +
-      NUM_OF_ENTRIES +
-      "&order=relevance&q=" +
-      RANDOM_KEYWORD +
-      "&type=video&key=" +
-      API_KEY
-  )
-    .then((response) => response.json())
-    .then((response) => response.items);
+    // fetch data from YouTube API
+    // based on relevance and top NUM_OF_ENTRIES
+    let data = await fetch(
+      "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=" +
+        NUM_OF_ENTRIES +
+        "&order=relevance&q=" +
+        RANDOM_KEYWORD +
+        "&type=video&key=" +
+        API_KEY
+    )
+      .then((response) => response.json())
+      .then((response) => response.items);
 
-
-  // select random 5 entries
-  let selectedData;
-  if (data !== undefined && data.length >= NUM_OF_ENTRIES) {
-    const shuffled = [...data].sort(() => 0.5 - Math.random());
-    const firstFiveShuffled = shuffled.slice(0, 5);
-    selectedData = [...firstFiveShuffled];
-  }
-
-  // format API Data into array
-  let results: {
-    title: string;
-    subtitle: string;
-    thumbnail: string;
-    url: string;
-  }[] = [];
-
-  if (selectedData !== undefined) {
-    for (let i = 0; i < selectedData.length; i++) {
-      let query = { title: "", subtitle: "", thumbnail: "", url: "" };
-      query.title = selectedData[i].snippet.title;
-      query.subtitle =
-        selectedData[i].snippet.channelTitle.length > SHORTEN_SUBTITLE
-          ? selectedData[i].snippet.channelTitle.substring(0, SHORTEN_SUBTITLE) + "..."
-          : selectedData[i].snippet.channelTitle;
-      query.thumbnail = selectedData[i].snippet.thumbnails.high.url;
-      query.url =
-        "https://www.youtube.com/watch?v=" + selectedData[i].id.videoId;
-      results.push(query);
+    // select random 5 entries
+    let selectedData;
+    if (data !== undefined && data.length >= NUM_OF_ENTRIES) {
+      const shuffled = [...data].sort(() => 0.5 - Math.random());
+      const firstFiveShuffled = shuffled.slice(0, 5);
+      selectedData = [...firstFiveShuffled];
     }
-  }
 
-  return results;
+    // format API Data into array
+    let results: {
+      title: string;
+      subtitle: string;
+      thumbnail: string;
+      url: string;
+    }[] = [];
+
+    if (selectedData !== undefined) {
+      for (let i = 0; i < selectedData.length; i++) {
+        let query = { title: "", subtitle: "", thumbnail: "", url: "" };
+        query.title = selectedData[i].snippet.title;
+        query.subtitle =
+          selectedData[i].snippet.channelTitle.length > SHORTEN_SUBTITLE
+            ? selectedData[i].snippet.channelTitle.substring(
+                0,
+                SHORTEN_SUBTITLE
+              ) + "..."
+            : selectedData[i].snippet.channelTitle;
+        query.thumbnail = selectedData[i].snippet.thumbnails.high.url;
+        query.url =
+          "https://www.youtube.com/watch?v=" + selectedData[i].id.videoId;
+        results.push(query);
+      }
+    }
+
+    return results;
+  } catch (error) {
+    console.error(error);
+  }
 }
